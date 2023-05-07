f18ef6794d722bfcf7e73488776711aaa7090337
SyabAhmad <syabblogger@gmail.com>
2023-05-07 12:19:12 +0500

    movie recommender system started

---
+++ README.md
@@ -0,0 +1,4 @@
+# Movie Recommender System in Python
+## Data used in this system 
+https://www.kaggle.com/datasets/sankha1998/tmdb-top-10000-popular-movies-dataset?resource=download
+###  you can download this dataset from kaggle website and start your practice.
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+# Loading Data and importing important imports
+import pandas as pd
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Now loading data
+movies = pd.read_csv("data/moviesData.csv")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Data Pre-Processing started
+# will display the data to check
+print(movies.head())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
